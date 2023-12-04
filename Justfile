f90e62180e8749408f4d1984fc0012c40eb604a2
Justin <justin@yan.family>
2023-12-03 21:43:12 -0800

    A first test implementation of py-wrapping just

---
+++ Justfile
@@ -0,0 +1,21 @@
+NAME:='just'
+VERSION:='1.16.0'
+PYPI_VERSION:='1.16.0a1'
+URL:='https://github.com/casey/just/releases'
+
+
+@default:
+    just --list
+
+@init:
+    [ -f Pipfile.lock ] && echo "Lockfile already exists" || PIPENV_VENV_IN_PROJECT=1 pipenv lock
+    PIPENV_VENV_IN_PROJECT=1 pipenv sync --dev
+
+@build: init
+    pipenv run python build_wheels.py --name {{NAME}} --version {{VERSION}} --pypi_version {{PYPI_VERSION}} --url {{URL}}
+
+@register:
+    git diff --name-only HEAD^1 HEAD -G"^VERSION:=" "Justfile" | uniq | xargs -I {} sh -c 'just _register'
+
+@_register: init build
+    pipenv run twine upload -u $PYPI_USERNAME -p $PYPI_PASSWORD dist/*
---
+++ build_wheels.py
@@ -0,0 +1,137 @@
+import argparse
+import io
+import os
+import tarfile
+import urllib.request
+import urllib.request
+from email.message import EmailMessage
+from pathlib import Path
+from zipfile import ZipInfo
+
+from wheel.wheelfile import WheelFile
+
+#######
+### Define mapping of archive name to pypi platform tag
+#######
+PLATFORMS = {
+    # https://packaging.python.org/en/latest/specifications/platform-compatibility-tags/
+    'aarch64-apple-darwin': 'macosx_11_0_arm64',
+    'aarch64-unknown-linux-musl': 'manylinux_2_17_aarch64.manylinux2014_aarch64.musllinux_1_1_aarch64',
+    'x86_64-apple-darwin': 'macosx_10_9_x86_64',
+    'x86_64-unknown-linux-musl': 'manylinux_2_12_x86_64.manylinux2010_x86_64.musllinux_1_1_x86_64',
+}
+#######
+### End mapping section
+#######
+
+
+def make_message(headers, payload=None):
+    msg = EmailMessage()
+    for name, value in headers.items():
+        if isinstance(value, list):
+            for value_part in value:
+                msg[name] = value_part
+        else:
+            msg[name] = value
+    if payload:
+        msg.set_payload(payload)
+    return msg
+
+
+def write_wheel_file(filename, contents):
+    with WheelFile(filename, 'w') as wheel:
+        for member_info, member_source in contents.items():
+            wheel.writestr(member_info, bytes(member_source))
+    return filename
+
+
+def convert_archive_to_wheel(
+        name: str,
+        pypi_version: str,
+        archive: bytes,
+        platform_tag: str
+):
+    contents = {}
+    contents[f'py{name}/__init__.py'] = b''
+    contents[f'py{name}/entrypoint.py'] = f'''\
+import subprocess
+import sys
+import os
+
+def main() -> None:
+    completed_process = subprocess.run(
+        [os.path.join(os.path.dirname(__file__), "alt{name}"), *sys.argv[1:]]
+    )
+    sys.exit(completed_process.returncode)
+
+if __name__ == "__main__":
+    main()
+'''.encode('ascii')
+
+    # Extract the command binary
+    with tarfile.open(mode="r:gz", fileobj=io.BytesIO(archive)) as tar:
+        for entry in tar:
+            if entry.isreg():
+                if entry.name.split('/')[-1] == f"{name}":
+                    zip_info = ZipInfo(f'py{name}/alt{entry.name}')
+                    zip_info.external_attr = ((entry.mode | (1 << 15)) & 0xFFFF) << 16
+                    contents[zip_info] = tar.extractfile(entry).read()
+
+    package_name = f'py{name}'
+    tag = f'py3-none-{platform_tag}'
+    metadata = {'Summary': '',
+                'Description-Content-Type': 'text/markdown',
+                'License': 'MIT',
+                'Classifier': [
+                    'License :: OSI Approved :: MIT License',
+                ],
+                'Requires-Python': '~=3.5'}
+    description = ''
+    wheel_name = f'{package_name}-{pypi_version}-{tag}.whl'
+    dist_info = f'{package_name}-{pypi_version}.dist-info'
+    outdir = "dist"
+    Path(outdir).mkdir(exist_ok=True)
+    return write_wheel_file(os.path.join(outdir, wheel_name), {
+        **contents,
+        f'{dist_info}/METADATA': make_message({
+            'Metadata-Version': '2.1',
+            'Name': package_name,
+            'Version': pypi_version,
+            **metadata,
+        }, description),
+        f'{dist_info}/WHEEL': make_message({
+            'Wheel-Version': '1.0',
+            'Generator': f'py{name} build_wheels.py',
+            'Root-Is-Purelib': 'false',
+            'Tag': tag,
+        }),
+        f'{dist_info}/entry_points.txt': f"""\
+[console_scripts]
+{name} = py{name}.entrypoint:main""".encode('ascii'),
+    })
+
+
+def build_wheel(name: str, base_url: str, version: str, pypi_version: str, target: str, platform_tag: str):
+    ######
+    ### Begin: Custom URL formatting
+    ######
+    full_url = f"{base_url}/download/{version}/{name}-{version}-{target}.tar.gz"
+    ######
+    ### End: Custom URL formatting
+    ######
+
+    with urllib.request.urlopen(full_url) as response:
+        archive = response.read()
+
+    convert_archive_to_wheel(name, pypi_version, archive, platform_tag)
+
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--name')
+    parser.add_argument('--version')
+    parser.add_argument('--pypi_version')
+    parser.add_argument('--url')
+    args = parser.parse_args()
+    for download_target, pypi_platform_tag in PLATFORMS.items():
+        build_wheel(args.name, args.url, args.version, args.pypi_version, download_target, pypi_platform_tag)
